c3dcd5e941de0dc4ecd59f0f9923366dfafc6007
Berdnikov Alexey <aberdnikov@gmail.com>
2017-05-10 14:38:05 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
@@ -8,20 +8,59 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
-            td {
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
                 text-align: center;
-                background: #ececec;
             }
-            td:first-child {
-                text-align: left;
-                background: #ffffff;
+
+            .title {
+                font-size: 84px;
             }
-            .img-thumbnail{
-                max-width: none;
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
             }
         </style>
     </head>
@@ -37,174 +76,20 @@
                     @endif
                 </div>
             @endif
-                <table class="table table-bordered">
-                    <tr>
-                        <th>
-                            Метод<br>(верт.)
-                        </th>
-                        <th width="10%">
-                            Оригинал<br>(верт.)
-                        </th>
-                        <th width="10%">
-                            Результат<br>(верт.)
-                        </th>
-                        <th width="10%">
-                            Оригинал<br>(гориз.)
-                        </th>
-                        <th width="10%">
-                            Результат<br>(гориз.)
-                        </th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <h4>HelperImage::resizeByWidth($original, 100);</h4>
-                            <br>
-                            Вписываем изображение в указанную ширину <br>
-                            Высота какая получится такая и будет<br>
-                            Пример: фотки на аватарках в контактике
-                        </td>
-                        <td>
-                            <strong>150x300</strong><br>
-                            <img class="img-thumbnail" src="/v.jpg">
-                        </td>
-                        <td>
-                            <strong>100x200</strong><br>
-                            <img class="img-thumbnail" src="/1_resizeByWidth_v.jpg">
-                        </td>
-                        <td>
-                            <strong>300x150</strong><br>
-                            <img class="img-thumbnail" src="/h.jpg">
-                        </td>
-                        <td>
-                            <strong>200x10</strong><br>
-                            <img class="img-thumbnail" src="/1_resizeByWidth_h.jpg">
 
-                        </td>
-                    </tr>
-                    {{--<tr>--}}
-                        {{--<td>--}}
-                            {{--<h4>HelperImage::resizeIgnoringAspectRatio($original, 100, 100);</h4>--}}
-                            {{--<br>--}}
-                            {{--Насильно вписываем изображение без учета пропорций в указанные рамки--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>150x300</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/2_resizeIgnoringAspectRatio_v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>300x150</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/h.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/2_resizeIgnoringAspectRatio_h.jpg">--}}
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
 
-                        {{--</td>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td>--}}
-                            {{--<h4>HelperImage::resizeImgInBox($img, $w, $h, $can_upsize);</h4>--}}
-                            {{--<br>--}}
-                            {{--Исходная картинка сжимается до тех пор пока не начнет целиком входить в указанные рамки c сохранением пропорций--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>150x300</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>50x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/3_resizeImgInBox_v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>300x150</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/h.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x50</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/3_resizeImgInBox_h.jpg">--}}
-
-                        {{--</td>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td>--}}
-                            {{--<h4>HelperImage::cropImgInBox($img, $w, $h, $can_upsize);</h4>--}}
-                            {{--<br>--}}
-                            {{--Уменьшаем размер исходного изображения с сохранением пропорций так,--}}
-                            {{--чтобы новое получилось вписанным в указанный размер. <br>--}}
-                            {{--Там где изображение уже отсутствует - добиваем белым цветом до указанного размера--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>150x300</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/4_cropImgInBox_v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>300x150</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/h.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/4_cropImgInBox_h.jpg">--}}
-
-                        {{--</td>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td>--}}
-                            {{--<h4>HelperImage::cropBoxInImg($img, $w, $h, $can_upsize);</h4>--}}
-                            {{--<br>--}}
-                            {{--Уменьшаем размер исходного изображения с сохранением пропорций так,чтобы новое получилось описанным вокруг указанного размера. <br>--}}
-                            {{--* Там где изображение будет за границами рамки оно будет просто обрезано с центровкой посредине картинки--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>150x300</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/5_cropBoxInImg_v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>300x150</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/h.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/5_cropBoxInImg_h.jpg">--}}
-
-                        {{--</td>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td>--}}
-                            {{--<h4>HelperImage::resizeBoxInImg($original, 100, 100);</h4>--}}
-                            {{--<br>--}}
-                            {{--Указанная рамка должна помещаться внутрь конечного изображения--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>150x300</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>200x100</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/6_resizeBoxInImg_v.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>300x150</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/h.jpg">--}}
-                        {{--</td>--}}
-                        {{--<td>--}}
-                            {{--<strong>100x200</strong><br>--}}
-                            {{--<img class="img-thumbnail" src="/6_resizeBoxInImg_h.jpg">--}}
-
-                        {{--</td>--}}
-                    {{--</tr>--}}
-                </table>
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
         </div>
     </body>
 </html>
